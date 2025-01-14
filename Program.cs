116b0667cc572cafa0de262d13e3b4a7ea2c806d
olikiverjesus <olikiversilva@gmail.com>
2025-01-14 00:01:59 -0300

    Initial commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Testando a classe Nokia
+            Console.WriteLine("Teste com Nokia:");
+            Smartphone nokia = new Nokia("123456789", "Nokia 3310", "123456789012345", 32);
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            // Testando a classe iPhone
+            Console.WriteLine("Teste com iPhone:");
+            Smartphone iphone = new Iphone("987654321", "iPhone 12", "987654321098765", 128);
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.WriteLine();
+        }
+    }
+}
